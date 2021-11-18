c1559542e02b6f4d7bcbf994251d4061e792869d
Felipe <fesenapeschi2013@gmail.com>
2021-11-18 06:30:36 -0300

    valida.php atualizado

--- php/valida.php
+++ php/valida.php
@@ -4,22 +4,40 @@ include('conection.php');
 
 $pass = $_POST['pass'];
 $email = $_POST['email'];
+$food = $_POST['food'];
 
 
+if(empty($_POST['food']) == true){
 
-$sql = "SELECT * FROM cadastro WHERE Email_cadastro = '$email' AND Senha_cadastro = '$pass'";
-$sql = $pdo->prepare($sql);
-$sql->execute();
+    $sql = "SELECT * FROM usuario WHERE Email_user = '$email' AND Senha_user = '$pass'";
+    $sql = $pdo->prepare($sql);
+    $sql->execute();
 
+        if ($sql->rowCount() > 0) {
+            
+            header("Location: ../index.html");
 
-
-if ($sql->rowCount() > 0) {
-    
-    header("Location: ../index.html");
+        }else{
+                echo "<script>alert('Dados Invalidos')</script>";
+                echo "<meta http-equiv='refresh' content='0.5;URL=../Login.html'>";
+            }
 }else{
-    echo "<script>alert('Dados Invalidos')</script>";
-    echo "<meta http-equiv='refresh' content='0.5;URL=../Login.html'>";
-  
+
+    $sql = "SELECT * FROM proprietario WHERE Email_prop = '$email' AND Senha_prop = '$pass' AND NomeEst_prop = '$food'";
+    $sql = $pdo->prepare($sql);
+    $sql->execute();
+    
+    
+    if ($sql->rowCount() > 0) {
+        
+        header("Location: ../painel.html");
+    }else{
+        echo "<script>alert('Dados Invalidos')</script>";
+        echo "<meta http-equiv='refresh' content='0.5;URL=../Login.html'>";
+      
+    }
+   
 }
+
 
 ?>
